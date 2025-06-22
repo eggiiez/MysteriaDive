2ec11a80a395c4cb0b183d40ec5b01222c71792d
eggiiez <6hysjacob@gmail.com>
2025-06-22 15:09:21 +0100

    Update crafter.js

--- assets/crafter.js
+++ assets/crafter.js
@@ -1,1 +1,21 @@
+let recipeData, namesData;
 
+document.addEventListener('DOMContentLoaded', ()=>{
+    const recipesUrl = "GDSRecipeData.json";
+    fetch(recipesUrl)
+    .then(response=>response.text())
+    .then(data=>{
+      const parsedRecipes = JSON.parse(data);
+      recipeData = parsedRecipes.m_dataMap;
+      console.log(recipeData); // temp: confirming data is valid.
+    });
+
+    const namesUrl = "GDSItemText_Noun.json";
+    fetch(namesUrl)
+    .then(response=>response.text())
+    .then(data=>{
+      const parsedNames = JSON.parse(data);
+      namesData = parsedNames.m_dataMap;
+      console.log(namesData);
+    });
+  })
